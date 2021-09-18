b6a8d2e6c5ef3567ec13136c8f96e607491811a6
ddelcid2 <89175881+ddelcid2@users.noreply.github.com>
2021-09-18 17:06:24 -0400

    Update
    added Images

--- Flixter/MoviesViewController.swift
+++ Flixter/MoviesViewController.swift
@@ -7,6 +7,7 @@
 
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
     
@@ -64,6 +65,15 @@ class MoviesViewController: UIViewController,  UITableViewDataSource, UITableVie
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl =  URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        
         
         
                     
